e15355a94ec00ba2cd87d3c357e5c7a26948da99
rzinke <rzinke@jpl.caltech.edu>
2025-06-18 09:41:23 -0700

    Fix indent

--- tools/bin/ariaPlot.py
+++ tools/bin/ariaPlot.py
@@ -182,7 +182,7 @@ def main(inps=None):
         # TODO make LHS a tuple
         standardproduct_info.products[0], standardproduct_info.products[1], \
             standardproduct_info.bbox_file, prods_TOTbbox, \
-            prods_TOTbbox_metadatalyr, arrres, proj, update_mode,
+            prods_TOTbbox_metadatalyr, arrres, proj, update_mode, \
             is_nisar_file = \
                 ARIAtools.extractProduct.merged_productbbox(
                     standardproduct_info.products[0],
